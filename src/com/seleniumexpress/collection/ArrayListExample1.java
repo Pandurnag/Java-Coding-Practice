2ad6c101a46445b3973ab2bb22cbfd70f3376f8b
Pandurnag <96949058+Pandurnag@users.noreply.github.com>
2022-06-10 15:17:38 +0530

    Java coding practice

---
+++ src/com/seleniumexpress/collection/ArrayListExample1.java
@@ -0,0 +1,22 @@
+package com.seleniumexpress.collection;
+
+import java.util.ArrayList;
+
+public class ArrayListExample1 {
+	
+	public static void main(String[] args) {
+		
+		ArrayList<String> list = new ArrayList<>();
+		
+		list.add("mango");
+		list.add("manan");
+		list.add("maju");
+		list.add("makuu");
+		list.add("makal");
+		
+		list.indexOf(4);
+		System.out.println(list);
+		
+	}
+
+}
